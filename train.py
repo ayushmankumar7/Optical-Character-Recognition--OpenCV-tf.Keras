906831a55be31edb5af8d28424fa75897eed9e5f
ayushmankumar7 <ayushmankumar7@gmail.com>
2019-12-28 11:11:50 +0530

    train modified

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import data_preprocess
 def fit_model():
 
     X_train, y_train, X_test, y_test = data_preprocess.data_preprocessed()
-
     mo = model.myModel()
 
     history = mo.fit(X_train, y_train, epochs = 10)
