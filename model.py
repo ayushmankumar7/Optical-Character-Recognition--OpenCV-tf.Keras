0b0b6e78db857e8b69e3d68cffd75acc7e5c7909
ayushmankumar7 <ayushmankumar7@gmail.com>
2019-12-29 13:19:51 +0530

    hyperparameter optimize

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ def myModel():
     model.add(Flatten())
     model.add(Dense(32, activation = 'relu'))
     model.add(Dense(64, activation = 'relu'))
-    model.add(Dense(16, activation = 'sigmoid'))
+    model.add(Dense(16, activation = 'relu'))
     model.add(Dense(10, activation= "softmax"))
 
     model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
